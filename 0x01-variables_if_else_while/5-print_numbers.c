162e2c7f348d5e0ccc469c368dacae1e331a610b
cecilia0411 <ceciliammkhabela04@gmail.com>
2023-07-14 13:49:58 -0700

    Prints numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
