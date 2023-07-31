97e136f598d8ac29243a8410151cbfb499efd5dd
cecilia0411 <ceciliammkhabela04@gmail.com>
2023-07-31 09:52:07 -0700

    One Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,16 +4,18 @@
  * _strchr - prints from the first occurrence of a char.
  * @s: source string
  * @c: character
- * Return: pointer to occurence of c or null
+ * Return: s or null
  */
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
 }
