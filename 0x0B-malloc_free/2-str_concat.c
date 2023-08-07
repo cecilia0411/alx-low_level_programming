4f2d9a5e2cacbc805b09dc181d145157b925a527
cecilia0411 <ceciliammkhabela04@gmail.com>
2023-08-07 12:02:45 -0700

    Str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,32 +9,23 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i;
-	char *con;
+	char *a;
+	int s1cnt, s2cnt, sizeBuff, i;
 
-	i = 0;
-	size1 = 0;
-	size1 = 0;
-
+	/*Check for valid strings*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	while (*(s1 + size1))
-		size1++;
-	while (*(s2 + size2))
-		size2++;
-	size2++; /*add 1 space to print null*/
-
-	con = malloc((size1 + size2) * sizeof(char));
-	if (con == NULL)
+	for (s1cnt = 0; s1[s1cnt]; s1cnt++)
+		;
+	for (s2cnt = 0; s2[s2cnt]; s2cnt++)
+		;
+	sizeBuff = s1cnt + s2cnt + 1;
+	p = malloc(sizeBuff * sizeof(char));
+	if (a == NULL)
 		return (NULL);
-
-	for (i = 0; i < size1; i++)
-		*(con + i) = *(s1 + i);
-	for (i = size1; i < (size1 + size2); i++)
-		*(con + i) = *(s2 + i - size1);
-
-	return (con);
+	for (i = 0; i < sizeBuff; i++)
+		i < s1cnt ? (a[i] = s1[i]) : (a[i] = s2[i - s1cnt]);
+	return (a);
 }
