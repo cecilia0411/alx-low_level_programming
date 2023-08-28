9c2a87b8f92e757e869daa56942007b24058a345
cecilia0411 <ceciliammkhabela04@gmail.com>
2023-08-28 04:48:43 -0700

    All elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h:the head address pointer
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *ptr;
+
+	if (h == NULL)
+		return (0);
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
