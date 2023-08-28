38067f461612839e7865da5d1d5240f7104d244c
cecilia0411 <ceciliammkhabela04@gmail.com>
2023-08-28 12:37:54 -0700

    Number of elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h:the head address pointer
- * Return: number of nodes
+ * Return: the number of nodes
  */
 
 size_t print_listint(const listint_t *h)
