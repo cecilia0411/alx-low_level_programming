2a886fb640903b724abdafb976c8b978e49d8ce6
cecilia0411 <ceciliammkhabela04@gmail.com>
2023-09-28 13:44:10 -0700

    Bin print

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,13 @@
 void print_binary(unsigned long int n)
 {
 	int i, count = 0;
-	unsigned long int current;
+	unsigned long int binar;
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = n >> i;
+		binar = n >> i;
 
-		if (current & 1)
+		if (binar & 1)
 		{
 			_putchar('1');
 			count++;
